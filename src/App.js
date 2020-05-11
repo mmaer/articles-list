2d73096ced60722fb36a566927ab469a07a0d2f8
Kamil Kruczek <kamilkruczek0@hotmail.com>
2020-05-11 18:18:53 +0200

    Configured eslint, prettier and nvmrc.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div className="app">
-      Hello World
-    </div>
-  );
+  return <div className="app">Hello World</div>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
+const { HotModuleReplacementPlugin } = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist',
-        port: 3000
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './index.html',
-    })],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            },
-        ]
-    }
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+    ]
+  }
 };
