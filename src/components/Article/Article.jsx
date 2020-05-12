73d3b347cf3cab03bdbcdef1ca008fed4056c3dc
Kamil Kruczek <kamilkruczek0@hotmail.com>
2020-05-12 20:53:20 +0200

    Improved Article component.

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -4,21 +4,21 @@ import imageNotFound from '../../images/image-not-found.png'
 
 import './article.scss';
 
-const Article = ({ date, image, title, premable }) => {
-  return (
-    <div className="article">
-      <div className="article__image">
-        <img src={image ? image : imageNotFound} alt="nature" />
-      </div>
-      <div className="article__content">
+const Article = ({ date, image, title, preamble }) => (
+  <div className="article">
+    <div className="article__image">
+      <img src={image ? image : imageNotFound} alt={title} />
+    </div>
+    <div className="article__content">
+      <div className="article__title-date">
         <div className="article__title">
           <h2>{title}</h2>
         </div>
         <div className="article__date">{date}</div>
-        <div className="article__text">{premable}</div>
       </div>
+      <div className="article__text">{preamble}</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Article;
